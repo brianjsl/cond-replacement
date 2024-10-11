integrate history guidance and fix numerical bug

algorithms/diffusion_forcing/df_numerical.py
@@ -44,16 +44,21 @@ class DiffusionForcingNumerical(DiffusionForcingBase):
     
     def on_validation_epoch_end(self, namespace="validation") -> None:
         loss_stats = []
-        for i, out in enumerate(self.validation_step_outputs):
-            for rg in self.cfg.diffusion.reconstruction_guidance:
-                (_, xs_pred, loss) = out[rg]
-                if rg != 0:
-                    self._visualize(xs_pred, name=f"{namespace}_vis/distribution_{i}_{rg}")
-                else:
-                    self._visualize(xs_pred, name=f'{namespace}_vis/distribution_{i}')
-                loss_stats.append(loss)
-                self.log_dict({f"{namespace}/loss_{rg}": torch.stack(loss_stats).mean()})
-                self.validation_step_outputs = []
+        for out in self.validation_step_outputs:
+            (_, xs_pred, loss) = out
+
+            type_name = ''
+            if self.is_conditional:
+                type_name += '_conditional'
+            elif self.cfg.diffusion.reconstruction_guidance != 0.0:
+                type_name += f'_rg_{self.cfg.diffusion.reconstruction_guidance[0]}'
+            else:
+                type_name += '_no_rg'
+
+            self._visualize(xs_pred, name=f'{namespace}_vis/distribution'+ type_name)
+            loss_stats.append(loss)
+            self.log_dict({f"{namespace}/loss"+type_name: torch.stack(loss_stats).mean()})
+        self.validation_step_outputs = []
 
     def on_test_epoch_end(self, namespace="test") -> None:
         self.on_validation_epoch_end(namespace)
@@ -70,7 +75,7 @@ class DiffusionForcingNumerical(DiffusionForcingBase):
         t = np.linspace(0, 1, steps)
         plt.clf()
         plt.xlim(*self.logging.x_lim)
-        plt.ylim(*self.logging.y_lim)
+        # plt.ylim(*self.logging.y_lim)
         # cmap = cm.get_cmap("coolwarm")
         for i in range(batch_size):
             color = "b" if xs_start[i] > 0 else "r"
@@ -85,44 +90,111 @@ class DiffusionForcingNumerical(DiffusionForcingBase):
     def validation_step(self, batch, batch_idx, namespace="validation") -> STEP_OUTPUT:
         xs, conditions, masks, *_ = batch
 
-        rg_losses = {}
+        context_guidance = [-self.cfg.history_guidance_scale]
+        context_guidance += [
+            (1 + self.cfg.history_guidance_scale) / self.n_context_tokens
+        ] * self.n_context_tokens
 
-        for rg in self.cfg.diffusion.reconstruction_guidance:
-            if self.is_conditional:
-                xs_pred, _= self._sample_sequence(
-                    xs.shape[1],
-                    xs.shape[0],
-                    None,
-                    None,
-                    conditions,
-                    None,
-                    rg 
-                )
-            else:
-                xs_pred, _ = self.predict_sequence(
-                    xs[: self.n_context_tokens],
-                    len(xs),
-                    conditions,
-                    reconstruction_guidance=rg,
-                    compositional=self.is_compositional,
+        if self.is_conditional:
+            xs_pred, _= self._sample_sequence(
+                xs.shape[1],
+                xs.shape[0],
+                None,
+                None,
+                conditions,
+                None,
+                self.cfg.diffusion.reconstruction_guidance
+            )
+        else:
+            xs_pred, _ = self.predict_sequence(
+                xs[: self.n_context_tokens],
+                len(xs),
+                conditions,
+                reconstruction_guidance=self.cfg.diffusion.reconstruction_guidance,
+                context_guidance=context_guidance,
+                compositional=self.is_compositional,
             )
 
-            # FIXME: loss
-            loss = self.loss_fn(xs_pred, xs, reduction="none")
-            loss = self._reweight_loss(loss, masks).detach().cpu()
+        # FIXME: loss
+        loss = self.loss_fn(xs_pred, xs, reduction="none")
+        loss = self._reweight_loss(loss, masks).detach().cpu()
 
+
+        if self.is_conditional:
             xs_copy = self._unstack_and_unnormalize(xs).detach().cpu()
             xs_pred_copy = self._unstack_and_unnormalize(xs_pred).detach().cpu()
+            conditions_vis = rearrange(conditions, "t b c -> (t c) b 1").contiguous().detach().cpu()
+            xs_copy = torch.cat([conditions_vis, xs_copy])
+            xs_pred_copy = torch.cat([conditions_vis, xs_pred_copy])
 
-            if self.is_conditional:
-                conditions_vis = rearrange(conditions, "t b c -> (t c) b 1").contiguous().detach().cpu()
-                xs_copy = torch.cat([conditions_vis, xs_copy])
-                xs_pred_copy = torch.cat([conditions_vis, xs_pred_copy])
+            self.validation_step_outputs.append((xs_copy,xs_pred_copy,loss))
+        else:
+            xs = self._unstack_and_unnormalize(xs).detach().cpu()
+            xs_pred = self._unstack_and_unnormalize(xs_pred).detach().cpu()
+            self.validation_step_outputs.append((xs, xs_pred, loss))
+            
+        return loss
 
-                rg_losses[rg] = (xs_copy,xs_pred_copy,loss)
-            else:
-                rg_losses[rg] = (xs, xs_pred, loss)
+# from typing import Optional, Any
+# from omegaconf import DictConfig
+# import numpy as np
+# from random import random
+# import torch
+# import torch.nn as nn
+# import wandb
+# from einops import rearrange, repeat, reduce
+# from PIL import Image, ImageFile
+# import matplotlib.pyplot as plt
+# import matplotlib.cm as cm
+# import os
+# from pytorch_lightning.utilities import rank_zero_only
 
-        self.validation_step_outputs.append(rg_losses)
+# from .df_base import DiffusionForcingBase
 
-        return rg_losses
+# class DiffusionForcingNumerical(DiffusionForcingBase):
+#     def __init__(self, cfg: DictConfig):
+#         super().__init__(cfg)
+
+#     def training_step(self, batch, batch_idx):
+#         output = super().training_step(batch, batch_idx)
+#         xs_pred = output["xs_pred"]
+#         xs = output["xs"]
+
+#         # visualize 64 samples
+#         if self.global_step % self.logging.train_vis_freq == 0:
+#             n = self.logging.train_vis_samples
+#             self._visualize(xs_pred[:, :n], name="training_vis/pred_distribution")
+#             self._visualize(xs[:, :n], name="training_vis/gt_distribution")
+
+#         return output
+
+#     def on_validation_epoch_end(self, namespace="validation") -> None:
+#         loss_stats = []
+#         for i, (_, xs_pred, loss) in enumerate(self.validation_step_outputs):
+#             self._visualize(xs_pred, name=f"{namespace}_vis/distribution_{i}")
+#             loss_stats.append(loss)
+#         self.log_dict({f"{namespace}/loss": torch.stack(loss_stats).mean()})
+#         self.validation_step_outputs = []
+
+#     @rank_zero_only
+#     def _visualize(self, xs, name):
+#         # xs ~ (t b c)
+#         xs = xs.detach().cpu().numpy()
+#         xs_start = xs[0, :, 0]
+#         subsample = 4
+#         xs = xs[::subsample, :, 0]
+#         steps, batch_size = xs.shape
+
+#         t = np.linspace(0, 1, steps)
+#         plt.clf()
+#         plt.xlim(*self.logging.x_lim)
+#         plt.ylim(*self.logging.y_lim)
+#         cmap = cm.get_cmap("coolwarm")
+#         for i in range(batch_size):
+#             color = "b" if xs_start[i] > 0 else "r"
+#             # color = xs[0, i] + 0.5
+#             plt.plot(t, xs[:, i], c=color, alpha=0.2)
+
+#         image_path = f'/tmp/numerical.png'
+#         plt.savefig(image_path)
+#         self.logger.experiment.log({name: wandb.Image(image_path)})

datasets/numerical/function1d.py
@@ -154,6 +154,21 @@ class ExponentialDataset(DoubleConeDataset):
             y = (-np.exp(-k * t * self.alpha)+1)/(2 * np.exp(self.alpha)) - self.epsilon
         return y
     
+class BimodalDataset(DoubleConeDataset):
+    def __init__(self, cfg: DictConfig, split: str = "training"):
+        super().__init__(cfg, split)
+        self.alpha = cfg.alpha
+
+    def base_function(self, t: np.ndarray) -> np.ndarray | float:
+        up = np.random.random() > 0.5
+        theta = np.random.random() 
+        b = (1 if theta > 0.5 else -1)*0.5 + np.random.random()
+        y = 2*(b-(1 if up else -1))*t + (1 if up else -1)
+        k = np.random.random()+2
+        y[self.context_length:] = (1 if theta > 0.5 else -1)*np.exp(k * self.alpha * 
+        (t[self.context_length:] - t[self.context_length]))+ (-1 if theta > 0.5 else 1) +y[self.context_length-1]
+        return y
+    
 
 class DiagonalDataset(DoubleConeDataset):
 
@@ -195,16 +210,16 @@ if __name__ == "__main__":
             "bazier_degree": 15,
             "purturbation": 0.01,
             "spike_multiplier": 20,
-            "alpha": 4,
+            "alpha": 1,
             "conditional": False,
             "external_cond_dim": False,
             "context_length": 100
         }
     )
-    # split = "training"
-    split = "validation"
-    dataset = DoubleConeDataset(cfg, split=split)
-    dataloader = DataLoader(dataset, batch_size=50)
+    split = "training"
+    # split = "validation"
+    dataset = BimodalDataset(cfg, split=split)
+    dataloader = DataLoader(dataset, batch_size=20)
     for d in dataloader:
         for y in d["xs"][:, :, 0]:
             t = np.linspace(0, 1, len(y))